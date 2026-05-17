8378427aef7784f9d819b7ed6cac9f1058693b77
reinasford57-dev <reinasford57@gmail.com>
2026-05-17 13:32:50 +0700

    Bismillah

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ client = OpenAI(
 )
 
 # Model yang kita pakai (gratis & stabil)
-MODEL_NAME = "deepseek/deepseek-r1-0528:free"
+MODEL_NAME = "deepseek/deepseek-chat-v3-0324:free"
 
 @app.get("/", response_class=HTMLResponse)
 async def get_ui():
